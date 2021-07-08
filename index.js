f5da52a8f3e0a8f19d566c6c6f2d9fa6f3d501a0
Needa Mehmud <needa.mehmud@gmail.com>
2021-07-07 23:14:31 -0400

    wip- working on questions

--- index.js
+++ index.js
@@ -6,41 +6,19 @@ const fs = require('fs');
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer.prompt([
+    {
+        type: 'input',
+        message: 'What is your Github Username?',
+        username: 'username'
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    }
+])
+    .then((response) => {
+        (console.log(response))
+        fs.writeFile('README.md', JSON.stringify, (res, null, '\t'), (err) =>
+            // TODO: Describe how this ternary operator works
+            err ? console.error(err) : console.log('Success!')
+        )
+    })
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
-
-
-// inquirer
-//     .prompt([
-//         {
-//             type: 'input',
-//             message: 'what is your name?',
-//             name: 'name',
-//         },
-//         {
-//             type: 'checkbox',
-//             message: 'what languages do you know?',
-//             name: 'techstack',
-//             choices: ['HTML', 'CSS', 'JAVASCRIPT', 'MySQL'],
-//         },
-//         {
-//             type: 'list',
-//             message: 'What is your preferred method of communication?',
-//             choices: ['email', 'phone', 'text'],
-
-//         },
-//     ])
-//     .then((response) => {
-//         (console.log(response))
-//         fs.writeFile('log.txt', JSON.stringify, (res, null, '\t'), (err) =>
-//             // TODO: Describe how this ternary operator works
-//             err ? console.error(err) : console.log('Success!')
-//     });
