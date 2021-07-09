ec873229d6ad82b9eaffa86f6815df061f20532f
Needa Mehmud <needa.mehmud@gmail.com>
2021-07-08 23:39:41 -0400

    wip - trying to resolve bugs

--- index.js
+++ index.js
@@ -1,24 +1,100 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+const generateREADME = (userInput) => {
+    return `# ${userInput.projectTitle}
+    
+ ## [Description](#project-description)
 
+  #### Table of Contents
+  1. [Installation](#installation-instructions)
+  2. [Usage](#usage-information)
+  3. [License](#license)
+  4. [Contributing](#contributor-guidelines)
+  5. [Tests](#test-instructions)
+  6. [Questions](#questions)
 
+  ## Description
+  * ${userInput.description}
+  ## Installation
+  * To install necessary dependencies, run the following command: ${userInput.install}
+  ## Usage 
+  * ${userInput.use}
+  ## License
+  * This project is licensed under the ${userInput.license}
+  ## Contributing
+  * ${userInput.contributions}
+  ## Tests
+  * To run test, run the following command: ${userInput.test}
+  
+  ## Questions
+  * If you have any questions about the rep, open an issue or contact me directly at  ${userInput.email}
+  * You can find more of my work at [${userInput.github}](http://github.com/${userInput.github})`;
 
-// TODO: Create an array of questions for user input
+}
+
 inquirer.prompt([
     {
-        type: 'input',
-        message: 'What is your Github Username?',
-        username: 'username'
+        type: "input",
+        name: "github",
+        message: "What is your Github username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your e-mail address?"
+    },
+    {
+        type: "input",
+        name: "project_title",
+        message: "What is your project name?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please write a short description of your project"
+    },
+    {
+        type: "checkbox",
+        message: "What kind of license should your project have?",
+        name: "license",
+        choices: [
+            "MIT License",
+            "APACHE 2.0 License",
+            "GPL 3.0 License",
+            "BSD 3 License",
+            "None",
+        ],
+    },
+    {
+        type: "input",
+        name: "dependencies",
+        message: "What command should be run to install the dependencies?"
 
-    }
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "What command should be run to run tests?"
+
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "What does the user need to know about using the repo?"
+    },
+    {
+        type: "input",
+        name: "contributions",
+        message: "What does the user need to know about contributing to the repo?"
+    },
 ])
-    .then((response) => {
-        (console.log(response))
-        fs.writeFile('README.md', JSON.stringify, (res, null, '\t'), (err) =>
-            // TODO: Describe how this ternary operator works
-            err ? console.error(err) : console.log('Success!')
-        )
-    })
+    .then((userInput) => {
+        const generateREADMEmd = generateREADME(userInput);
+
+        fs.writeFile('README.md', generateREADMEmd, (err) =>
+            err ? console.log(err) : console.log('YAY! you have created a README.md!'));
+    });
+;
+
 
